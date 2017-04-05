80a3361fc182df4e47258197fa1cf97d05172fb3
chadwick2143 <chenliang2143@qq.com>
2017-04-06 00:18:28 +0800

    add file

---
+++ date.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+class Date
+{
+    int year, month, day;
+    
+    public:
+    Date(int y=2017, int m=1, int d=1)
+    {
+        year = y;
+        month = m;
+        day = d;
+    }
+    ~Date(){}
+    int GetYear()
+    {
+        cout << "year " << year << endl;
+        return 0;
+    }
+    int GetMonth()
+    {
+        cout << "month " << month << endl;
+        return 0;
+    }
+    int GetDay()
+    {
+        cout << "day " << day << endl;
+        return 0;
+    }
+    void ShowDate()
+    {
+        cout << year << "/" << month << "/" << day << endl;
+    }
+};
+int main()
+{
+    int y, m, d;
+    cin >> y >> m >> d;
+    Date date(y, m, d);
+    date.ShowDate();
+    date.GetYear();
+    date.GetMonth();
+    date.GetDay();
+    
+    return 0;
+}
